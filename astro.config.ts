35f9ab3833b029278c36a00537c5ae468337e7eb
Clément Nerma <clement.nerma@gmail.com>
2023-01-19 10:23:20 +0100

    Use a TypeScript config file for Astro

---
+++ astro.config.ts
@@ -0,0 +1,14 @@
+/** @file https://astro.build/config */
+
+import { defineConfig } from 'astro/config'
+
+import node from '@astrojs/node'
+import solidJs from '@astrojs/solid-js'
+
+export default defineConfig({
+	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
+	integrations: [solidJs()],
+})
