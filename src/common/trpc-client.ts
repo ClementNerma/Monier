80322a6d92191fd00bb713a8b9bbdd9944258925
Clément Nerma <clement.nerma@gmail.com>
2023-01-24 20:28:08 +0100

    Use 'superjson' for Date type in tRPC

--- src/common/trpc-client.ts
+++ src/common/trpc-client.ts
@@ -1,8 +1,10 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
 import type { AppRouter } from '../server'
+import superjson from 'superjson'
 
 export function createApiClient(url: string) {
 	return createTRPCProxyClient<AppRouter>({
+		transformer: superjson,
 		links: [
 			httpBatchLink({
 				url: `${url}/trpc`,
