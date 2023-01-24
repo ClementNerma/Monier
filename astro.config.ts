63f9dc0d99a42eb0c85443626b88637b94f90ffb
Clément Nerma <clement.nerma@gmail.com>
2023-01-24 16:37:47 +0100

    Add TailwindCSS

--- astro.config.ts
+++ astro.config.ts
@@ -4,13 +4,14 @@ import { defineConfig } from 'astro/config'
 import node from '@astrojs/node'
 import vue from '@astrojs/vue'
 import { map } from './src/common/utils'
+import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
 	output: 'server',
 	adapter: node({
 		mode: 'standalone',
 	}),
-	integrations: [vue()],
+	integrations: [vue(), tailwind()],
 	server: {
 		port: map(process.env['CURRENT_SERVER_PORT'], parseInt) ?? 7288,
 	},
